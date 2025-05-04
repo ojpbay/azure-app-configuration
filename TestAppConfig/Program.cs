5386f73c244fb705bac06e587f2b7f4945fb47ad
ojpbay <oli.baylis@live.co.uk>
2025-05-04 14:59:50 +0100

    Add dynamic app configuration by following https://learn.microsoft.com/en-us/azure/azure-app-configuration/enable-dynamic-configuration-aspnet-core

--- TestAppConfig/Program.cs
+++ TestAppConfig/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
 using TestAppConfig;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,10 +8,21 @@ string connectionString = builder.Configuration.GetConnectionString("AppConfigur
     ?? throw new InvalidOperationException("The connection string 'AppConfiguration' was not found.");
 
 // Load configuration from Azure App Configuration
-builder.Configuration.AddAzureAppConfiguration(connectionString);
+builder.Configuration.AddAzureAppConfiguration(options =>
+{
+    options.Connect(connectionString)
+           // Load all keys that start with `TestApp:` and have no label.
+           .Select("TestApp:*", LabelFilter.Null)
+           // Reload configuration if any selected key-values have changed.
+           .ConfigureRefresh(refreshOptions =>
+            refreshOptions.RegisterAll());
+});
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+// Add Azure App Configuration middleware to the container of services.
+builder.Services.AddAzureAppConfiguration();
 
 // Bind configuration "TestApp:Settings" section to the Settings object
 builder.Services.Configure<Settings>(builder.Configuration.GetSection("TestApp:Settings"));
@@ -24,6 +36,9 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+// Use Azure App Configuration middleware for dynamic configuration refresh.
+app.UseAzureAppConfiguration();
 
 app.UseHttpsRedirection();
 
